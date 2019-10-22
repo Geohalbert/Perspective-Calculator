aad73c62549c807865ae77c52f9a02155c7fcce1
Hal Peters <geohalbert@gmail.com>
2019-10-22 03:35:29 -0500

    setting up firestore

--- functions/admin.js
+++ functions/admin.js
@@ -1,9 +1,4 @@
 const admin = require('firebase-admin');
-var serviceAccount = require('/Users/halpeters/Programming/keys/persp-calc/perspective-calc-firebase-adminsdk-jhfrt-d219fe772d.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://perspective-calc.firebaseio.com"
-});
+admin.initializeApp();
 
 module.exports = admin;
