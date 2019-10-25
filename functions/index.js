a8f6345e2351a98c6e8dbbd9381c7b9661447b5e
Hal Peters <geohalbert@gmail.com>
2019-10-25 05:54:27 -0500

    cloud function for default

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
-const onUserCreated  					= require('./onUserCreated.js');
+const onUserCreated	= require('./onUserCreated.js');
+const defaultCollections = require('./defaultCollections.js')
 
 /* Functions */
 exports.onUserCreated = onUserCreated;
 
 /* HTTP Functions */
+exports.defaultCollections = defaultCollections;
 
 /* PubSubs */
