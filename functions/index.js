8f98c95edb3f656c49786b2fb3989e1f0d076c6e
Hal Peters <geohalbert@gmail.com>
2019-10-31 06:53:43 -0500

    user update trigger

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-const functions = require('firebase-functions');
 const onUserCreated	= require('./onUserCreated.js');
+const onUserUpdated	= require('./onUserUpdated.js');
 const getCollections = require('./getCollections.js')
 
 /* Functions */
 exports.onUserCreated = onUserCreated;
+exports.onUserUpdated = onUserUpdated;
 
 /* HTTP Functions */
 exports.defaultCollections = getCollections.default
---
+++ functions/onUserUpdated.js
@@ -0,0 +1,13 @@
+const functions = require('firebase-functions');
+const admin = require('./admin');
+
+const db = admin.firestore()
+
+module.exports = functions.firestore.document('objects/users/{userId}/{userObject}').onUpdate((change, context) => {
+    const user = context.params.userId
+    const userProfile = db.doc(`users/${user}`);
+    const timestamp = new Date().getTime()
+    return userProfile.update({
+        updatedAt: timestamp
+    });
+});
