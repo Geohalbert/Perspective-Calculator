e4a613256fbcb48c160261df1ca11a56bb494f79
Hal Peters <geohalbert@gmail.com>
2019-10-31 09:29:13 -0500

    new trigger function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,12 @@
 const onUserCreated	= require('./onUserCreated.js');
 const onUserUpdated	= require('./onUserUpdated.js');
-const getCollections = require('./getCollections.js')
+const onCollectionCreated = require('./onCollectionCreated.js')
+const getCollections = require('./getCollections.js');
 
 /* Functions */
 exports.onUserCreated = onUserCreated;
 exports.onUserUpdated = onUserUpdated;
+exports.onCollectionCreated = onCollectionCreated;
 
 /* HTTP Functions */
 exports.defaultCollections = getCollections.default
